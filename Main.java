4478fb1bd31d47707df462505c4e45ee2b722048
Ethem ÇAĞLAR <142922479+EthemCaglar@users.noreply.github.com>
2023-09-29 13:50:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package Mine;
+
+public class Main {
+    public static void main(String[] args) {
+        MayinTarlasinda mineSweeper = new MayinTarlasinda();
+        mineSweeper.minesweeper();
+    }
+}
