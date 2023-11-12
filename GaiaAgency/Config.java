3f8bc9af4ed30e63e583833bf2e8e25221533aa7
Ethem ÇAĞLAR <142922479+EthemCaglar@users.noreply.github.com>
2023-11-13 00:03:49 +0300

    Add files via upload

---
+++ GaiaAgency/Config.java
@@ -0,0 +1,9 @@
+package com.gaiaagency.Helper;
+
+public class Config {
+    public static final String PROJECT_TITLE = "Gaia Agency";
+    public static final String DB_URL = "jdbc:mysql://localhost/hotelbase";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "mysql";
+}
+
