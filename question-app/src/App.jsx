e731a5134c94c2a5c46d46eb66a192817e729af9
Ethem Çağlar <caglarethem@gmail.com>
2024-02-28 22:19:20 +0300

    question app added

---
+++ question-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { useState } from 'react'
+import Question from './components/Question'
+import { questions } from './questions'
+
+
+function App() {
+  const [questions, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ question-app/src/components/Question/index.js
@@ -0,0 +1,2 @@
+import Person from "./Question";
+export default Question;
